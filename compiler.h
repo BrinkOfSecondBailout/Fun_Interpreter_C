e68219cb7cadb1ccff9a118912c826ba1a149889
Danny <nguyenduongpa@gmail.com>
2025-03-21 20:44:27 -0700

    parse precedence

--- compiler.h
+++ compiler.h
@@ -3,6 +3,27 @@
 
 #include "vm.h"
 
+typedef struct {
+    Token previous;
+    Token current;
+    bool hadError;
+    bool panicMode;
+} Parser;
+
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT,  // =
+    PREC_OR,          // or
+    PREC_AND,         // and
+    PREC_EQUALITY,    // == !=
+    PREC_COMPARISON,  // < > <= >=
+    PREC_TERM,        // + -
+    PREC_FACTOR,      // * /
+    PREC_UNARY,       // ! -
+    PREC_CALL,        // . ()
+    PREC_PRIMARY
+} Precedence;
+
 bool compile(const char *source, Chunk *chunk);
 
 #endif
